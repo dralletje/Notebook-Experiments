76156cd5898f4440d3cf86d83e0a9a940e5b9a83
Michiel Dral <m.c.dral@gmail.com>
2022-11-06 23:07:29 +0100

    HMM

--- web-app/iframe/render-cells.jsx
+++ web-app/iframe/render-cells.jsx
@@ -1,5 +1,12 @@
-import { Inspector } from "@observablehq/inspector";
+import { Inspector } from "../src/Inspector.jsx";
 import React from "react";
+
+/** @param {import("../src/notebook-types").CylinderShadow} cylinder */
+let Cell = (cylinder) => {
+  console.log(`cylinder:`, cylinder);
+
+  return <Inspector value={cylinder.result} />;
+};
 
 export let NotebookOutput = ({ cells }) => {
   return <div>Hello!!</div>;
