47c3e469588d89e39814ef0d9ea8d6c2988d109b
Michiel Dral <m.c.dral@gmail.com>
2023-04-02 13:19:41 +0200

    Katex in shadowdom

---
+++ simple-notebooks/web-app/src/packages/codemirror-markdown-wysiwyg/parts/katex/adoptedStylesheets.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+declare global {
+  interface Node {
+    getRootNode(): ShadowRoot | Document;
+  }
+  interface ShadowRoot {
+    adoptedStyleSheets: CSSStyleSheet[];
+  }
+  interface Document {
+    adoptedStyleSheets: CSSStyleSheet[];
+  }
+}
+
+export class CSSish {
+  id = `STYLESHEET-` + String((Math.random() * 10 ** 10).toFixed());
+  style_text: string;
+  sheet: CSSStyleSheet | undefined;
+
+  constructor(style_text) {
+    this.style_text = style_text;
+    try {
+      let sheet = new CSSStyleSheet();
+      sheet.replaceSync(style_text);
+      this.sheet = sheet;
+    } catch (error) {
+      console.log(`error:`, error);
+    }
+  }
+}
+
+export let adopt_at = (element: HTMLStyleElement, stylesheet: CSSish) => {
+  let shadow_root = element.getRootNode();
+  if (stylesheet.sheet == null) {
+    if (shadow_root.querySelector(`#${stylesheet.id}`) != null) return;
+
+    element.id = stylesheet.id;
+    element.textContent = stylesheet.style_text;
+    return () => {
+      element.id = "";
+      element.textContent = "";
+    };
+  } else {
+    let sheet = stylesheet.sheet;
+    shadow_root.adoptedStyleSheets = [
+      ...(shadow_root.adoptedStyleSheets ?? []),
+      sheet,
+    ];
+
+    return () => {
+      let index = shadow_root.adoptedStyleSheets.indexOf(sheet);
+      shadow_root.adoptedStyleSheets = [
+        ...shadow_root.adoptedStyleSheets.slice(0, index),
+        ...shadow_root.adoptedStyleSheets.slice(index + 1),
+      ];
+    };
+  }
+};
+
+export let AdoptStylesheet = ({ stylesheet }: { stylesheet: CSSish }) => {
+  let ref = React.useRef(null as HTMLStyleElement | null);
+  React.useLayoutEffect(() => {
+    if (ref.current == null) return;
+    let element = ref.current;
+    return adopt_at(element, stylesheet);
+  }, []);
+  return <style ref={ref} />;
+};
