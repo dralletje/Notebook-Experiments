e3c28127262f1586bf8c495b0706c060a03e169f
Michiel Dral <m.c.dral@gmail.com>
2023-04-02 17:52:05 +0200

    @vitejs/plugin-react-swc

--- simple-notebooks/web-app/vite.config.js
+++ simple-notebooks/web-app/vite.config.js
@@ -1,6 +1,6 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
