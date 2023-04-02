45c53817707b65cf2ce817b154075ce9ccdc61cc
Michiel Dral <m.c.dral@gmail.com>
2023-04-02 20:02:02 +0200

    #2

--- lezer-playground/src/App.jsx
+++ lezer-playground/src/App.jsx
@@ -36,6 +36,26 @@ import {
   DEFAULT_TO_PARSE,
 } from "./default-field-codes.js";
 import "./App.css";
+import { useCodemirrorKeyhandler } from "../../simple-notebooks/web-app/src/use/use-codemirror-keyhandler";
+import { JavascriptStuffEditor } from "./editors/javascript-editor/javascript-editor";
+import { LezerEditor } from "./editors/lezer-editor/lezer-editor";
+
+import { compact, isEmpty, round } from "lodash-es";
+import {
+  create_nested_editor_state,
+  EditorInChief,
+  EditorInChiefKeymap,
+  extract_nested_viewupdate,
+} from "./should-be-shared/codemirror-editor-in-chief/editor-in-chief";
+import {
+  shared_history,
+  historyKeymap,
+  historyField,
+} from "./should-be-shared/codemirror-editor-in-chief/codemirror-shared-history";
+import { lezerLanguage } from "@codemirror/lang-lezer";
+import { iterate_over_cursor } from "dral-lezer-helpers";
+import { LezerErrorEditor } from "./editors/lezer-editor/lezer-error-editor";
+import { AppHeader } from "./Header.jsx";
 
 /**
  * @template T
@@ -545,94 +565,7 @@ let AppScroller = styled.div`
   scroll-snap-type: x mandatory;
 `;
 
-import { compact, head, isEmpty, range, round, sortBy, uniq } from "lodash-es";
-import {
-  create_nested_editor_state,
-  EditorInChief,
-  EditorInChiefKeymap,
-  extract_nested_viewupdate,
-} from "./should-be-shared/codemirror-editor-in-chief/editor-in-chief";
-import {
-  shared_history,
-  historyKeymap,
-  historyField,
-} from "./should-be-shared/codemirror-editor-in-chief/codemirror-shared-history";
-import { lezerLanguage } from "@codemirror/lang-lezer";
-import { iterate_over_cursor } from "dral-lezer-helpers";
-import { cool_cmd_d } from "./should-be-shared/commands.js";
-import { DecorationsFromTree } from "@dral/codemirror-helpers";
-
 let lezer_playground_storage = new ScopedStorage("lezer-playground");
-
-// A very dim/dull syntax highlighting so you have something to look at, but also to trigger you to write your own ;)
-// Also shows that you can use `export let extension = [...]`, to add extensions to the "demo text" editor.
-import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
-import { tags as t } from "@lezer/highlight";
-const syntax_colors = syntaxHighlighting(
-  HighlightStyle.define(
-    [
-      { tag: t.name, color: "#b10000" },
-      { tag: t.propertyName, color: "#ff0000", fontWeight: "bold" },
-      { tag: t.atom, color: "#ffffff", fontWeight: 700 },
-      { tag: t.self, color: "#ffffff", fontWeight: 700 },
-
-      { tag: t.literal, color: "#99ad00" },
-      { tag: t.moduleKeyword, color: "white", fontWeight: "bold" },
-    ],
-    { all: { color: "#5f0000" } }
-  )
-);
-
-import { parser as lezer_error_parser } from "@dral/lezer-lezer-error";
-import { useCodemirrorKeyhandler } from "../../simple-notebooks/web-app/src/use/use-codemirror-keyhandler.js";
-import { base_extensions } from "./editors/shared";
-import { JavascriptStuffEditor } from "./editors/javascript-editor/javascript-editor.jsx";
-import { LezerEditor } from "./editors/lezer-editor/lezer-editor.jsx";
-
-let lezer_error_lang = new LanguageSupport(
-  LRLanguage.define({
-    // @ts-ignore
-    parser: lezer_error_parser,
-  })
-);
-
-let extension_for_error = (error_message) => {
-  console.log(`error_message:`, error_message);
-  if (error_message.startsWith("shift/reduce conflict between")) {
-    console.log("SHIFT REDUCE");
-    return [
-      EditorView.theme({
-        "&": {
-          color: "red",
-        },
-      }),
-    ];
-  } else {
-    return [
-      EditorView.theme({
-        "&": {
-          color: "red",
-        },
-      }),
-    ];
-  }
-};
-
-let LezerErrorEditor = ({ error }) => {
-  let initial_editor_state = React.useMemo(() => {
-    return EditorState.create({
-      doc: error.message,
-      extensions: [
-        base_extensions,
-        extension_for_error(error.message),
-        lezer_error_lang,
-        syntax_colors,
-      ],
-    });
-  }, [error.message]);
-
-  return <CodeMirror state={initial_editor_state} />;
-};
 
 let PaneSelect = styled.select`
   border: none;
@@ -1340,55 +1273,12 @@ let Editor = ({ project_name }) => {
             gridArea: "header",
           }}
         >
-          <InnerHeaderLol>
-            <span
-              style={{
-                flex: 1,
-                alignSelf: "stretch",
-                display: "flex",
-                alignItems: "stretch",
-              }}
-            >
-              <ProjectsDropdown />
-              <LoadSampleDropdown scoped_storage={main_scope} />
-              <ExtraDropdown scoped_storage={main_scope} />
-            </span>
-            <span style={{ fontWeight: "bold" }}>Lezer Playground</span>
-            <span
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "flex-end",
-              }}
-            >
-              <SimpleLink href="https://github.com/dralletje">
-                <IoHeart
-                  title="By Michiel Dral (link to github)"
-                  style={{ fontSize: 16 }}
-                />
-              </SimpleLink>
-              <div style={{ width: 8 }} />
-              <SimpleLink href="https://github.com/dralletje/Notebook-Experiments/tree/main/lezer-playground">
-                <IoLogoGithub
-                  title="Github Repository"
-                  style={{ fontSize: 16 }}
-                />
-              </SimpleLink>
-            </span>
-          </InnerHeaderLol>
+          <AppHeader main_scope={main_scope} />
         </div>
       </AppGrid>
     </AppScroller>
   );
 };
-
-let SimpleLink = styled.a`
-  cursor: pointer;
-  &:hover {
-    color: #00e1ff;
-  }
-`;
 
 let Runtime = styled.div`
   font-size: 0.9em;
@@ -1396,278 +1286,3 @@ let Runtime = styled.div`
   opacity: 0.7;
   align-self: flex-end;
 `;
-
-let ProjectDropdownStyle = styled.div`
-  position: relative;
-
-  button {
-    border: none;
-    height: 100%;
-    padding: 0 8px;
-    font-weight: normal;
-
-    body:not(.mouse-down) &:hover {
-      background-color: white;
-      color: black;
-    }
-  }
-
-  .menu {
-    display: flex;
-    background-color: black;
-    min-width: 150px;
-    font-size: 16px;
-
-    flex-direction: column;
-
-    border: solid 4px white;
-
-    a {
-      padding: 8px 16px;
-      white-space: pre;
-      cursor: pointer;
-      font-family: var(--mono-font-family);
-
-      border-top: solid white 2px;
-      &:first-child {
-        border-top: none;
-      }
-
-      &.active {
-        cursor: unset;
-        color: #37ff61;
-        font-weight: bold;
-        background-color: #323232;
-      }
-
-      &:not(.active):hover {
-        background-color: white;
-        color: black;
-      }
-    }
-  }
-
-  .help {
-    width: 300px;
-    background-color: black;
-    font-size: 16px;
-    padding: 16px;
-    border: solid 4px white;
-    border-left: none;
-  }
-
-  .dropdown {
-    display: none;
-    position: absolute;
-    top: 100%;
-    left: 0;
-
-    flex-direction: row;
-
-    @media (max-width: 450px) {
-      position: fixed;
-      top: 30px;
-      left: 0;
-      right: 0;
-    }
-  }
-
-  body:not(.mouse-down) &:has(button:hover),
-  &:has(.dropdown:hover) {
-    .dropdown {
-      display: flex;
-    }
-    button {
-      background-color: white;
-      color: black;
-    }
-  }
-`;
-
-/** @param {Storage} storage */
-let storage_keys = function* (storage) {
-  for (let i of range(0, storage.length)) {
-    let key = storage.key(i);
-    if (key != null) {
-      yield key;
-    }
-  }
-};
-
-let ProjectsDropdown = () => {
-  let path = window.location.pathname;
-
-  let project_names = sortBy(
-    uniq(
-      Array.from(storage_keys(localStorage))
-        .map((x) => x.split("."))
-        .filter((x) => x[0] === "lezer-playground")
-        .map((x) => x[1])
-    )
-  );
-
-  return (
-    <ProjectDropdownStyle>
-      <button>projects</button>
-      <div className="dropdown">
-        <div className="menu">
-          {project_names.map((project_name) => (
-            <a
-              className={path === project_name ? "active" : ""}
-              key={project_name}
-              href={project_name}
-            >
-              {project_name}
-            </a>
-          ))}
-        </div>
-        <div className="help">
-          To open a new project, change the path to something not listed here.
-        </div>
-      </div>
-    </ProjectDropdownStyle>
-  );
-};
-
-let path_prefix = "./premade-projects/";
-// @ts-expect-error - Vite glob 😎
-const modules = import.meta.glob("./premade-projects/**/*", { as: "raw" });
-let premade_projects = {};
-for (let [path, import_module] of Object.entries(modules)) {
-  let [project, filename] = path.slice(path_prefix.length).split("/");
-  premade_projects[project] ??= {};
-
-  if (filename.startsWith("example.")) {
-    premade_projects[project].example = import_module;
-  } else if (filename === "external.js") {
-    premade_projects[project].external = import_module;
-  } else if (filename.endsWith(".grammar")) {
-    premade_projects[project].grammar = import_module;
-  } else {
-    console.error(`Unknown file type: ${path}/${filename}`);
-  }
-}
-
-let LoadSampleDropdown = ({ scoped_storage }) => {
-  let path = window.location.pathname;
-  let project_names = Object.keys(premade_projects);
-
-  return (
-    <ProjectDropdownStyle>
-      <button>load</button>
-      <div className="dropdown">
-        <div className="menu">
-          {project_names.map((project_name) => (
-            <a
-              key={project_name}
-              onClick={() => {
-                Promise.all([
-                  premade_projects[project_name].example(),
-                  premade_projects[project_name].grammar(),
-                  premade_projects[project_name].external(),
-                ]).then(([example, grammar, external]) => {
-                  scoped_storage.child("code_to_parse").set(example),
-                    scoped_storage.child("javascript_stuff").set(external),
-                    scoped_storage.child("parser_code").set(grammar),
-                    window.location.reload();
-                });
-              }}
-            >
-              {project_name}
-            </a>
-          ))}
-        </div>
-        <div className="help">
-          Load an existing parser.
-          <br />
-          <br />
-          I've compiled those from github, mashing the javascript files
-          together. Maybe in the future I'll make a way to actually import from
-          github.
-          <br />
-          <br />
-          <b>This will override the project you have open currently!</b>
-        </div>
-      </div>
-    </ProjectDropdownStyle>
-  );
-};
-
-let InnerHeaderLol = styled.div`
-  width: 100%;
-  height: 100%;
-  max-width: calc(100vw - 16px);
-  position: sticky;
-  right: 8px;
-  left: 8px;
-
-  display: flex;
-  align-items: center;
-
-  user-select: none;
-  font-size: 12px;
-`;
-
-let ExtraDropdown = ({ scoped_storage }) => {
-  let project_names = sortBy(
-    uniq(
-      Array.from(storage_keys(localStorage))
-        .map((x) => x.split("."))
-        .filter((x) => x[0] === "lezer-playground")
-        .map((x) => x[1])
-    )
-  );
-
-  return (
-    <ProjectDropdownStyle>
-      <button style={{ color: "gray" }}>other</button>
-      <div className="dropdown">
-        <div className="menu">
-          <a
-            onClick={() => {
-              scoped_storage.child("history").remove();
-              window.location.reload();
-            }}
-          >
-            clear project history
-          </a>
-          <a
-            onClick={() => {
-              for (let project_name of project_names) {
-                let storage = lezer_playground_storage
-                  .child(project_name)
-                  .child("history");
-                storage.remove();
-              }
-              window.location.reload();
-            }}
-          >
-            clear <b style={{ color: "#b20000" }}>all history</b>
-          </a>
-          <a
-            onClick={() => {
-              if (window.confirm("Hey, are you sure?")) {
-                for (let child of scoped_storage.children()) {
-                  console.log(`child.key:`, child.key);
-                  child.remove();
-                }
-                window.location.href = "/";
-              }
-            }}
-          >
-            remove project
-          </a>
-        </div>
-        <div className="help">
-          I needed a catch all where I can put all my other miscellaneous
-          options.
-          <br />
-          <br />
-          Delete history is here because I save the codemirror history to
-          localstorage, but it turns out localstorage has a limited size, so I
-          needed a way to clear the history if it gets too big.
-        </div>
-      </div>
-    </ProjectDropdownStyle>
-  );
-};
