f4e502f3c96a9b9d17d081a2958c0bd871f89632
Michiel Dral <m.c.dral@gmail.com>
2023-04-02 12:46:56 +0200

    Fak yeah

--- simple-notebooks/web-app/postcss.config.js
+++ simple-notebooks/web-app/postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: [
     require("autoprefixer"),
-    require("postcss-nesting"),
+    // @ts-ignore
+    require("tailwindcss/nesting")("postcss-nesting"),
+    // require("postcss-nesting"),
     require("tailwindcss"),
   ],
 };
